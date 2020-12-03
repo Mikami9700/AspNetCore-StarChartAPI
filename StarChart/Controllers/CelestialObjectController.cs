f56e755556f5d70edc01777e108255480289f930
Mikami9700 <mlouk9700@gmail.com>
2020-12-03 09:28:12 +0300

    Chapter 4 tasks 4 Completed

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using StarChart.Data;
 
 namespace StarChart.Controllers
 {
+    [Route("")]
+    [ApiController]
     public class CelestialObjectController : ControllerBase
     {
+        private readonly ApplicationDbContext _context;
 
-        public string Route = "";
-        public string ApiController;
+        public CelestialObjectController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
     }
 }
